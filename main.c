6bccbb8fc0e4f6d63b44ce72710b2a8f3ba33ccb
Iipal <malkoleyplay@gmail.com>
2018-09-26 11:01:59 +0300

    fixed uncheked return value from system()

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #define ECHO_RED(msg) "echo \"\033[0;31m" msg "\033[0m\""
+#define ERR_MSG "Something wrong.\nMaybe programm hasn't access to create something on your pc.\n"
+
+int		g_sys;
 
 enum    e_keys {
     DIRECTORY = 1,
@@ -20,20 +23,24 @@ void    ft_action(const char *action, const char *dir, const char *file)
         file ? snprintf(act, act_len, "%s%s/%s", action, dir, file) :
                 snprintf(act, act_len, "%s%s", action, dir);
         printf("$ %s\n", act);
-        system(act);
+        if ((g_sys = system(act)) == -1)
+			printf("%s", ERR_MSG);
     } else
-        system(ECHO_RED("mem err."));
+		if ((g_sys = system(ECHO_RED("mem err."))) == -1)
+			printf("%s", ERR_MSG);
     free(act);
 }
 
 int     main(int argc, char **argv)
 {
-    if (argc == 3)
+	g_sys = 0;
+	if (argc == 3)
     {
         ft_action("mkdir ", argv[DIRECTORY], NULL);
         ft_action("touch ", argv[DIRECTORY], argv[FILE_NAME]);
     } else if (argc == 2)
         ft_action("mkdir ", argv[DIRECTORY], NULL);
     else
-        system(ECHO_RED("not enought args."));
+		if ((g_sys =system(ECHO_RED("not enought args."))) == -1)
+			printf("%s", ERR_MSG);
 }
