b24695a880e6f4dffdd4f21e173b3a478f94bfb2
Iipal <malkoleyplay@gmail.com>
2018-10-12 02:39:39 +0300

    removed action func

--- lib/srcs/ft_noflags_parse.c
+++ lib/srcs/ft_noflags_parse.c
@@ -1,22 +1,3 @@
-void	ft_action(const string action, const string dir, const string file)
-{
-	string	act;
-	int		act_len;
-	int		dir_len;
-	int		file_len;
-
-	file_len = (file ? (strlen(file) + 1) : 0);
-	dir_len = strlen(dir);
-	act_len = strlen(action) + dir_len + file_len + 1;
-	STOP_IF((act = (string)malloc(sizeof(char) * act_len)) == NULL,
-			return , ERR_NOMEMORY);
-	file ? snprintf(act, act_len, "%s%s/%s", action, dir, file) :
-		snprintf(act, act_len, "%s%s", action, dir);
-	printf("$ %s\n", act);
-	system(act);
-	free(act);
-}
-
 void	ft_noflags_parse(int argc, string *argv)
 {
 	if (argc == 3)
